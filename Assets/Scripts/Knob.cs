349017e6bb1611caf98bd83b34888f7684cef288
Jacob Cheatley (AWARE) <jacob.cheatley@awaregroup.com>
2021-01-27 18:33:40 +1300

    Simple test project

---
+++ Assets/Scripts/Knob.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Knob : MonoBehaviour
+{
+    private SpriteRenderer r;
+    void Start()
+    {
+        r = GetComponent<SpriteRenderer>();
+    }
+    public void SetColor(string hex)
+    {
+        Color result;
+        if (ColorUtility.TryParseHtmlString(hex, out result))
+            r.color = result;
+    }
+}
