458b57d1343863f245adebb0ebf8bc8d2d477290
Jacob Cheatley (AWARE) <jacob.cheatley@awaregroup.com>
2021-01-29 15:12:38 +1300

    Simple day night switching begins

---
+++ Assets/Scripts/DayNightSwitcher.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    private Transform cameraAnchorDay;
+    [SerializeField]
+    private Transform cameraAnchorNight;
+
+    private bool day = true;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (day)
+                Night();
+            else
+                Day();
+        }
+    }
+
+    public void Day()
+    {
+        CameraControl.Follow(cameraAnchorDay);
+        day = true;
+    }
+
+    public void Night()
+    {
+        CameraControl.Follow(cameraAnchorNight);
+        day = false;
+    }
+}
